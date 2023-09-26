aedcf28e8c1b6ad6a2fb03433f7fe0662136bbfd
huynhvanlinh <huynhvanlinh1062003@gmail.com>
2023-09-27 00:05:48 +0700

    first commit

---
+++ src/normalizetext/NormalizeText.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package normalizetext;
+
+
+/**
+ *
+ * @author huynh
+ */
+public class NormalizeText {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+    
+}
